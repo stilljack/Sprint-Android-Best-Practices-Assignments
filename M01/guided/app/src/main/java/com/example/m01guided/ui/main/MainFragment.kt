c379d40150646d329f9207f110920da2057e5c41
jack seymour <stilljack@gmail.com>
2019-10-28 10:52:33 -0700

    always trying to commit while i'm not sure if it works....

--- M01/guided/app/src/main/java/com/example/m01guided/ui/main/MainFragment.kt
+++ M01/guided/app/src/main/java/com/example/m01guided/ui/main/MainFragment.kt
@@ -7,17 +7,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.m01guided.R
+import com.example.m01guided.model.ForexService
+import com.example.m01guided.model.Rates
+import com.jakewharton.rxbinding3.widget.textChanges
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.rxkotlin.toObservable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.main_fragment.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
-
+    val compositeDisposable = CompositeDisposable()
     private lateinit var viewModel: MainViewModel
+    val object1:Observable<Int> = Observable.create{emitter ->
+        for (i in 0 until 10) {
+            Thread.sleep(350)
+            emitter.onNext(i)
+        }
+        emitter.onComplete()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,16 +65,71 @@ class MainFragment : Fragment() {
         val obs2 = Observable.fromIterable(list)
 
         val obs3 = Observable.create<Int> { emitter ->
-            for (i in 0..500) {
-                emitter.onNext(i)
-            }
+            for (i in 0..20) {
+              GlobalScope.launch {
+                  emitter.onNext(i)
+              }
+              }
         }
-        obs3.subscribeBy (  // named arguments for lambda Subscribers
-            onNext = { println(it) },
-            onError =  { it.printStackTrace() },
-            onComplete = { println("Done!") }
-        )
+        var atomic = AtomicBoolean(false)
 
+        btn_main.setOnClickListener {
+
+            if (atomic.get() !=true) {
+                atomic.set(true)
+            val disposable =
+                    object1.subscribeOn(Schedulers.computation())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(  // named arguments for lambda Subscribers
+                            {
+                                println(it)
+                                tv_main.text = it.toString()
+                            },
+                            {atomic.set(false) },
+                            {
+                                atomic.set(false)
+                                println("Done!")
+                                tv_main.text = "done"
+                            }
+                        )
+
+
+            compositeDisposable.add(disposable)
+                }
+        }
+        val retrofy = Retrofit.Builder().baseUrl("https://api.exchangerate-api.com/v4/latest")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+
+        val service = retrofy.create(ForexService::class.java)
+
+        val newDisposable = service.getRates("USD").subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .retry(3)
+            .subscribe(
+                {t1: Rates?, t2: Throwable? ->
+                    et_one.setText(t1?.rates.toString())
+                }
+
+            )
+
+
+                val etonefield = et_one.textChanges()
+        val ettwofield = et_two.textChanges()
+       /* val obsFieldsCombined = Observable.combineLatest(etonefield,ettwofield) {first,last ->
+            "$first $last"
+        }
+*/
+    }
+
+
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 
 }
