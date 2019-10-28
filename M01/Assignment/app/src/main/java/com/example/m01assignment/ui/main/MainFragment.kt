5f7626b7f47fdeeeadfe29f82ac368850a2a075b
jack seymour <stilljack@gmail.com>
2019-10-28 12:38:13 -0700

    alright lets try canary and hopefully @compose + motionlayout

--- M01/Assignment/app/src/main/java/com/example/m01assignment/ui/main/MainFragment.kt
+++ M01/Assignment/app/src/main/java/com/example/m01assignment/ui/main/MainFragment.kt
@@ -6,10 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.m01assignment.MainActivity
 import com.example.m01assignment.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
+    lateinit var callback: OnWhateverListener
 
+    fun setOnWhateverListener(callback: OnWhateverListener) {
+        this.callback = callback
+    }
+    interface OnWhateverListener {
+
+        fun doAthing()
+    }
     companion object {
         fun newInstance() = MainFragment()
     }
@@ -20,13 +30,16 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setOnWhateverListener(callback)
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val something = activity
+        message.setOnClickListener { callback.doAthing() }
     }
 
 }
