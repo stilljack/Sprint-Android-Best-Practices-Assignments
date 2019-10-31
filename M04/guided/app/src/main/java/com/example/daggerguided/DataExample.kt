f0f2877398ab3c5567c171454fe913018875efb9
jack seymour <stilljack@gmail.com>
2019-10-31 10:51:10 -0700

    some how more opaque.
    good.

---
+++ M04/guided/app/src/main/java/com/example/daggerguided/DataExample.kt
@@ -0,0 +1,15 @@
+package com.example.daggerguided
+
+import java.util.*
+
+object DataExample {
+     private val date:Long  = Date().time
+
+
+
+
+
+    fun getDate(): Long {
+        return date
+    }
+}
